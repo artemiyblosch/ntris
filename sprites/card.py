2dc1781e1d962c4da0d1babcb21bf2727419b274
arti <arti@privategatevpn.com>
2025-05-29 20:38:26 +0300

    Layout fixed finally

--- sprites/card.py
+++ sprites/card.py
@@ -14,33 +14,32 @@ class Card(pg.sprite.Sprite):
         self.world = world
         self.mode_link = mode_link
         self.card_left, self.card_top, self.card_width, self.card_height = rect
-
         self.card_sprites = pg.sprite.Group()
         self.border = self.add(Border(rect,4))
-        self.sr_text = self.add(Text((self.card_top + 10, self.card_left + 50),
+        self.sr_text = self.add(Text((self.card_left + 10, self.card_top + 50),
                          "Size Range:",
                          "small"
                 ))
-        self.sra_text = self.add(Text((self.card_top + 10, self.card_left + 65),
+        self.play_button = self.add(Button(
+                    Rect_Layout(self.card_left + 10, self.card_top+self.card_height - 45, self.card_width - 20, 40),
+                    "Play",
+                    self.ch_mode_c(self.world[0]),
+                    border=4
+                )),
+        self.sra_text = self.add(Text((self.card_left + 10, self.card_top + 65),
                          f"{self.world[0].gen_range[0]}-{self.world[0].gen_range[1]}",
                          "small"
                 ))
-        self.max_text = self.add(Text((self.card_top + 10, self.card_left + 90),
+        self.max_text = self.add(Text((self.card_left + 10, self.card_top + 90),
                          f"Max Score:",
                          "small"
                 ))
-        self.score_text = self.add(Text((self.card_top + 10, self.card_left + 105),
+        self.score_text = self.add(Text((self.card_left + 10, self.card_top + 105),
                          f"{self.world[1]}",
                          "small"
                 ))
         self.del_button = self.add(Button(
-                    Rect_Layout(self.card_top + 10, self.card_top+self.card_height - 90, self.card_width - 20, 40),
-                    "Delete",
-                    self.delete_entry_c(self.world[0].sandbox_entry),
-                    border=4
-                ))
-        self.play_button = self.add(Button(
-                    Rect_Layout(self.card_top + 10, self.card_top+self.card_height - 90, self.card_width - 20, 40),
+                    Rect_Layout(self.card_left + 10, self.card_top+self.card_height - 90, self.card_width - 20, 40),
                     "Delete",
                     self.delete_entry_c(self.world[0].sandbox_entry),
                     border=4
@@ -57,6 +56,11 @@ class Card(pg.sprite.Sprite):
             self.world_cards = []
             self.init()
         return __
+    
+    def ch_mode_c(self, mode : Mode):
+        def __():
+            self.mode_link.set_as(mode)
+        return __
 
     def add(self,obj : pg.sprite.Sprite) -> pg.sprite.Sprite:
         self.card_sprites.add(obj)
@@ -68,6 +72,7 @@ class Card(pg.sprite.Sprite):
         self.sra_text.ch_text(color=col.selected)
         self.score_text.ch_text(color=col.selected)
         self.max_text.ch_text(color=col.selected)
+        self.play_button[0].select()
     
     def unselect(self):
         self.border.color = col.borders
@@ -75,5 +80,6 @@ class Card(pg.sprite.Sprite):
         self.sra_text.ch_text(color=col.borders)
         self.score_text.ch_text(color=col.borders)
         self.max_text.ch_text(color=col.borders)
+        self.play_button[0].unselect()
     
     def click_check(self, pos): pass
